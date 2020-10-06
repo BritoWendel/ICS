e99a343a7a20aca83801b2ea1e08cc05ea5369a7
João Gabriel Sabatini <user962@yahoo.com>
2020-10-06 18:24:09 -0300

    Limpeza/Conector do banco adicionado

--- src/ClientRegisterWindow.py
+++
@@ -1,9 +0,0 @@
-from CenteredWindow import *
-from ClientRegisterFrame import *
-
-class ClientRegisterWindow(CenteredWindow):
-    def __init__(self):
-        super().__init__(width=800, height=487, title='Cadastro de cliente')
-        app = ClientRegisterFrame(self)
-        app.pack(fill='both', padx=10, pady=10)
-
--- src/EntryRule.py
+++
@@ -1,5 +0,0 @@
-class EntryRule(object):
-    def __init__(self, pos, symbol):
-        self.position = pos
-        self.symbol = symbol
-
