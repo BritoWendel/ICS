63a5ca91b3970207ba1f706ab7cd679cb4ace595
wendellbrito <wendelrfbrito@gmail.com>
2020-10-10 20:19:55 -0300

    atualizada janela de notificações

--- src/NotificationWindow.py
+++ src/NotificationWindow.py
@@ -5,17 +5,28 @@ from tkinter import *
 class NotificationWindow(CenteredWindow):
     def __init__(self, message):
         super().__init__(600, 200, 'Notificação!')
-        
-        self.__frame = Frame(self)
-        self.__label_message = Label(self, text = message)
-        self.__label_message.pack()
+        #----------------------widgets--------------------
+        self.__label_message = Label(self, text = message, pady = 20)
+        self.__button_cancel = Button(self, text = 'Cancelar')
         self.__button_ok = Button(self, text = 'OK')
-        self.__button_ok.pack()
+
+        #------------------grid configure-----------------
+        self.grid_columnconfigure(index=0, minsize=1, weight=1)
+        self.grid_columnconfigure(index=1, minsize=1, weight=1)
+        self.grid_columnconfigure(index=2, minsize=1, weight=1)
+        self.grid_rowconfigure(index=0, minsize=1, weight=1)
+        self.grid_rowconfigure(index=1, minsize=1, weight=1)
+        self.grid_rowconfigure(index=2, minsize=1, weight=1)
+
+        #----------------------layout---------------------
+        self.__label_message.grid(row=0, column=0, columnspan=3)
+        self.__button_cancel.grid(row=2, column=0)
+        self.__button_ok.grid(row=2, column=2)
 
 
 #remove the " ''' " of the lines below to see this winddows runnig
-'''
+
 instance_notification = NotificationWindow('UM ERRO OCORREU!')
 
 instance_notification.mainloop()
-'''
+
