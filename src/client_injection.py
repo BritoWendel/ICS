61fff5e795647a49e09b6e9d771556fffdbae0d4
wendellbrito <wendelrfbrito@gmail.com>
2020-10-27 22:17:10 -0300

    atualizado maneira de injetar dados no deposito de dados

--- src/client_injection.py
+++ src/client_injection.py
@@ -1,7 +1,16 @@
 from Database import *
+from random import randint
+
+rsociais = ['Wendel Brito', 'Aline Aparecida', 'Andre Ricardo', 'Jeferson Luiz', 'João Gabriel', 'Rodney de Andrade', 'Tiffany Carvalho']
+cnpjs = ['68559950000194','24525844000114','95475845000140','74614401000178','68486686000106','77156187000141','99967166000149']
+iestaduais = ['737430566205','437625328589','855099423414','977927890277','353345380120','300560160705','055593537275']
 
 db = Database('dev', 'dev', 'localhost', 'sci_db')
 for i in range(200):
+    rsocial = rsociais[randint(0,6)]
+    iestadual = iestaduais[randint(0,6)]
+    cnpj = cnpjs[randint(0,6)]
+
     db.insert("CLIENTE", ['bairro_cliente',
             'cep_cliente',
             'rsocial_cliente',
@@ -17,22 +26,21 @@ for i in range(200):
             'complemento_cliente',
             'url_cliente',
             'id_municipio_cliente'],
-            ['New Maracanã',
+            ['Barão',
             '13453343',
-            'wendelbrito',
+            rsocial,
             '999995555',
             '19',
-            'wendel',
+            rsocial.lower().strip(" "),
             '0',
-            '10669134000153',
-            '592063602906',
+            cnpj,
+            iestadual,
             '99999994',
             'Rua manoel machado pereira',
-            'wendelbrito@email.com.br',
+            rsocial.lower().strip(" ") + '@email.com.br',
             'Nenhum',
-            'facebook.com/wendel',
-            '2']
-            )
+            'facebook.com/'+ rsocial.lower().strip(" "),
+            '4'])
     id_cliente = str(db.last_insert_id()[0]['LAST_INSERT_ID()'])
     db.insert("TELEFONE",
             ["ddd_telefone",
